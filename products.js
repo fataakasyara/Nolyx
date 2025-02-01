7447a8a45a3bb655e35e1e19b0e0e3c625915530
fakasy <140477415+fakasy@users.noreply.github.com>
2025-02-01 14:39:45 +0700

    Add files via upload

--- products.js
+++ products.js
@@ -21,6 +21,13 @@ const products = {
             image: 'https://res.cloudinary.com/teepublic/image/private/s--RGCQRNDN--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1737168483/production/designs/71283381_0.jpg',
             price: 23,
             buyLink: 'https://www.teepublic.com/t-shirt/71283381-eye-of-rah-memes-lock-in',
+        },
+        {
+            id: 'fishrockshirt',
+            title: 'There is Sea Under the Rock Shirt',
+            image: 'https://res.cloudinary.com/teepublic/image/private/s--NPRzTfB3--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,w_470/c_crop,g_north_west,h_626,w_470,x_0,y_0/g_north_west,u_upload:v1462829015:production:blanks:mtl53ofohwq5goqjo9ke,x_-395,y_-325/b_rgb:eeeeee/c_limit,f_auto,h_630,q_auto:good:420,w_630/v1738327228/production/designs/71804962_0.jpg',
+            price: 23,
+            buyLink: 'https://www.teepublic.com/t-shirt/71804962-there-is-sea-under-the-rock',
         }
     ],
     hoodies: [
